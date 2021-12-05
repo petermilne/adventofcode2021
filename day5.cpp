516fe39e89b47bfbfc85b49c0eefbc439e5fe305
Peter Milne <peter.milne@d-tacq.com>
2021-12-05 17:06:22 +0000

    day5: loads data OK
    [  0]   0,  9 ->   5,  9 HORIZ
    [  1]   8,  0 ->   0,  8 SLOPE
    [  2]   9,  4 ->   3,  4 HORIZ
    [  3]   2,  2 ->   2,  1 VERT
    [  4]   7,  0 ->   7,  4 VERT
    [  5]   6,  4 ->   2,  0 SLOPE
    [  6]   0,  9 ->   2,  9 HORIZ
    [  7]   3,  4 ->   1,  4 HORIZ
    [  8]   0,  0 ->   8,  8 SLOPE
    [  9]   5,  5 ->   8,  2 SLOPE
    
     On branch main
    	new file:   day5.cpp
    	new file:   day5_sample.dat

---
+++ day5.cpp
@@ -0,0 +1,90 @@
+/*
+ * day5.cpp
+ *
+ *  Created on: 5 Dec 2021
+ *      Author: pgm
+ *  Consider only horizontal and vertical lines. At how many points do at least two lines overlap?
+ *
+ * sample input:
+0,9 -> 5,9
+8,0 -> 0,8
+9,4 -> 3,4
+2,2 -> 2,1
+7,0 -> 7,4
+6,4 -> 2,0
+0,9 -> 2,9
+3,4 -> 1,4
+0,0 -> 8,8
+5,5 -> 8,2
+ */
+
+
+#include <string>
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <vector>
+
+#include <string.h>
+
+struct Point {
+	int x; int y;
+
+	Point(int _x, int _y): x(_x), y(_y) {}
+};
+
+struct Line {
+	Point p1; Point p2;
+	char str[128];
+
+	Line(int x1, int y1, int x2, int y2) : p1(x1, y1), p2(x2, y2) {
+		snprintf(str, 128, "%3d,%3d -> %3d,%3d", p1.x, p1.y, p2.x, p2.y);
+		std::cout << "new Line " << toString() << std::endl;
+	}
+
+	bool isHorizontal() const {
+		return p1.y == p2.y;
+	}
+	bool isVertical() const {
+		return p1.x == p2.x;
+	}
+	const char* toString() const {
+		return str;
+	}
+};
+
+
+typedef std::vector<Line*> VL;
+
+int get_data(std::istream& is, VL* lines)
+{
+	std::string current;
+	while (std::getline(is, current) && current.length() > 1){
+		int x1, y1, x2, y2;
+		std::cout << "line:"  << current << std::endl;
+		if (sscanf(current.c_str(), "%d,%d -> %d,%d", &x1, &y1, &x2, &y2) == 4){
+			std::cout << "scan good\n";
+			lines->push_back(new Line(x1, y1, x2, y2));
+		}
+		std::cout << "so far so good:" << lines->size() << "\n";
+	}
+	return lines->size();
+}
+
+
+
+int main(int argc, const char** argv)
+{
+	VL* lines = new VL;
+	int nlines = get_data(std::cin, lines);
+
+	std::cout << "get_data returned " << nlines << std::endl;
+
+	int ii = 0;
+	for (Line* l : *lines){
+		printf("[%3d] %s %s\n", ii++, l->toString(),
+				l->isHorizontal()? "HORIZ": l->isVertical()? "VERT": "SLOPE");
+	}
+}
+
+
